5e2de0e82cadcd547d3032e974c21fa242c1cd42
Derek Wheat <trigostruck@gmail.com>
2021-08-25 23:44:01 -0600

    login page is sending to spotify for auth!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
       <header className="app-header">
-          
+        <Login />
       </header>
     </div>
   );
